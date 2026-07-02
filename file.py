ddb8393b4cc697f8db1ee78053ec2e5282d3ad04
shambhu-creater <sgkumar7634@gmail.com>
2026-07-02 08:47:32 +0545

    add x

--- file.py
+++ file.py
@@ -1,5 +1,11 @@
-a=1
-i=2
-while a <= 10:
-     print(a**i)
-     a=a+1
+x=(1,4,9,16,25,36,49,64,81,100)
+b=25
+a=2
+i=1
+while i <= 10:
+     if(i**a==b):
+          print("found at index",i)
+     else:
+          print("not found at index",i)
+     i=i+1     
+     
