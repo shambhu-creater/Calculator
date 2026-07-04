41e3934a39e8d3716d1ba717e018f0845204b027
shambhu-creater <sgkumar7634@gmail.com>
2026-07-04 07:53:05 +0545

    add converter

--- file.py
+++ file.py
@@ -1,11 +1,7 @@
-x=(1,4,9,16,25,36,49,64,81,100)
-b=25
-a=2
+n=5
+a = 0
 i=1
-while i <= 10:
-     if(i**a==b):
-          print("found at index",i)
-     else:
-          print("not found at index",i)
-     i=i+1     
-     
+while  i<=n:
+    a = a + i
+    i=i+1
+print ("sum of number is" , a)
---
+++ file2.py
@@ -0,0 +1,4 @@
+def converter(usd_value):
+    np_value = usd_value * 152  # Example conversion
+    print(usd_value, "USD =", np_value, "NPR")
+converter(2)
